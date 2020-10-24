0605289a834b4f474b387de82e4c68833ba0bb08
Kevin Zhang <71031989+Hackysh@users.noreply.github.com>
2020-10-23 20:55:45 -0700

    Setup for files for odomcontrol
    File setup

---
+++ src/QuantumOdom/TwoTrackerOdom.cpp
@@ -0,0 +1,1 @@
+#include "QuantumOdom/TwoTrackerOdom.hpp"
---
+++ src/QuantumOdom/XDrive.cpp
@@ -0,0 +1,13 @@
+#include "QuantumOdom/XDrive.hpp"
+
+#include "api.h"
+
+
+class XDrive {
+	private:
+		pros::Motor rightFront;
+		pros::Motor rightBack;
+		pros::Motor leftBack;
+		pros::Motor leftFront;
+	public:
+};
